b27cad70ebda519bbcece89e75b5e69bf545a401
Arooj Ishfaq <talk2aroojishfaq@gmail.com>
2023-12-28 12:04:52 +0500

    test commit

---
+++ src/StoreContext.jsx
@@ -0,0 +1,46 @@
+// Init
+import React, { useContext, useState, createContext } from 'react';
+
+// Initializing Create Context Hook
+const storeContext = createContext();
+const storeUpdateContext = createContext();
+
+// We are also initializing useContexts here
+// we only call these functions where we need that data
+export function Store() {
+  return useContext(storeContext);
+}
+
+
+// const data=createContext();
+// const updateData=createContext();
+
+
+
+export function UpdateStore() {
+  return useContext(storeUpdateContext);
+}
+
+// Initializing Store Provider
+export function StoreProvider({ children }) {
+  // Initializing State
+  const [store, setStore] = useState({
+    loggedIn: false,
+    user: {},
+  });
+
+  // eslint-disable-next-line react/jsx-no-constructed-context-values
+  const updateStore = (data) => {
+    setStore((prev) => ({
+      ...prev,
+      ...data,
+    }));
+  };
+
+  // Render
+  return (
+    <storeContext.Provider value={store}>
+      <storeUpdateContext.Provider value={updateStore}>{children}</storeUpdateContext.Provider>
+    </storeContext.Provider>
+  );
+}
